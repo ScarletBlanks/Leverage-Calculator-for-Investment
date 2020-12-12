8df353b14bd4e8fc800f90bbf881e7fb40bb6eac
ScarletBlanks <irisu.nita@gmail.com>
2020-12-12 14:33:44 +0800

    Initial commit

---
+++ Investment/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Investment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.profitBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.leverageBox = new System.Windows.Forms.TextBox();
+            this.entryBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.exitBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // profitBox
+            // 
+            this.profitBox.Location = new System.Drawing.Point(111, 32);
+            this.profitBox.Name = "profitBox";
+            this.profitBox.Size = new System.Drawing.Size(76, 20);
+            this.profitBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "profit%";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "leverage x";
+            // 
+            // leverageBox
+            // 
+            this.leverageBox.Location = new System.Drawing.Point(111, 68);
+            this.leverageBox.Name = "leverageBox";
+            this.leverageBox.Size = new System.Drawing.Size(76, 20);
+            this.leverageBox.TabIndex = 3;
+            // 
+            // entryBox
+            // 
+            this.entryBox.Location = new System.Drawing.Point(111, 101);
+            this.entryBox.Name = "entryBox";
+            this.entryBox.Size = new System.Drawing.Size(76, 20);
+            this.entryBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "entry";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "exit";
+            // 
+            // exitBox
+            // 
+            this.exitBox.Location = new System.Drawing.Point(111, 132);
+            this.exitBox.Name = "exitBox";
+            this.exitBox.Size = new System.Drawing.Size(76, 20);
+            this.exitBox.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(219, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(87, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 191);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.exitBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.entryBox);
+            this.Controls.Add(this.leverageBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.profitBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox profitBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox leverageBox;
+        private System.Windows.Forms.TextBox entryBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox exitBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Investment/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Investment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Short");
+            comboBox1.Items.Add("Long");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            float exit;
+            float profitMul = float.Parse(profitBox.Text);
+            float leverageMul = float.Parse(leverageBox.Text);
+            float entryPrice = float.Parse(entryBox.Text);
+
+            if (comboBox1.Text == "Long"){
+                    exit = (entryPrice * (profitMul / leverageMul))/100 + entryPrice;
+                }
+                else if (comboBox1.Text == "Short")
+                {
+                    exit = entryPrice - (entryPrice * (profitMul / leverageMul))/100;
+                }
+                else
+                {
+                    exit = 0;
+                }
+                exitBox.Text = exit.ToString();
+
+            }
+            catch
+            {
+                MessageBox.Show("error");
+            }
+        }
+    }
+}
